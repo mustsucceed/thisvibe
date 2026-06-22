c3b3c2fd1978537914cb8f6fac4ad7916982998b
Abdulsalam <127743255+mustsucceed@users.noreply.github.com>
2026-06-22 12:16:35 +0100

    fail instead of hanging
    .

--- Server_files/mongoconnect.js
+++ Server_files/mongoconnect.js
@@ -9,4 +9,19 @@ const connectdb = async () => {
   console.log("Connected to MongoDB");
 };
 
+export default connectdb;import mongoose from "mongoose";
+
+const connectdb = async () => {
+  if (!process.env.MONGO_URL) {
+    throw new Error("MONGO_URL is missing from the environment");
+  }
+
+  await mongoose.connect(process.env.MONGO_URL, {
+    serverSelectionTimeoutMS: 10000, // fail after 10s instead of hanging
+    connectTimeoutMS: 10000,
+  });
+
+  console.log("Connected to MongoDB");
+};
+
 export default connectdb;
