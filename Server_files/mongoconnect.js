a60c973961c11fc2a2eb01562c9afd7a1c8570f6
Abdulsalam <127743255+mustsucceed@users.noreply.github.com>
2026-06-24 15:29:50 +0100

    Update mongoose
    ..

--- Server_files/mongoconnect.js
+++ Server_files/mongoconnect.js
@@ -5,17 +5,6 @@ const connectdb = async () => {
     throw new Error("MONGO_URL is missing from the environment");
   }
 
-  await mongoose.connect(process.env.MONGO_URL);
-  console.log("Connected to MongoDB");
-};
-
-export default connectdb;import mongoose from "mongoose";
-
-const connectdb = async () => {
-  if (!process.env.MONGO_URL) {
-    throw new Error("MONGO_URL is missing from the environment");
-  }
-
   await mongoose.connect(process.env.MONGO_URL, {
     serverSelectionTimeoutMS: 10000, // fail after 10s instead of hanging
     connectTimeoutMS: 10000,
